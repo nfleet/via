90ed26d3398c549294c82fbbaee8e36a603b9e1e
ane <ane@iki.fi>
2013-10-28 10:59:38 +0200

    Add definitions for bounding boxes.

---
+++ definitions.go
@@ -0,0 +1,16 @@
+package main
+
+var bounding_boxes = map[string]map[string]float64{
+	"finland": map[string]float64{
+		"long_min": 20.54,     // Border between Swe-Nor-Fin
+		"long_max": 31.5867,   // Somewhere in Ilomantsi
+		"lat_min":  59.807983, // Hanko
+		"lat_max":  70.092283, // Nuorgam
+	},
+	"germany": map[string]float64{
+		"long_min": 5.8666667, // Isenbruch, Nordrhein-Westfalen
+		"long_max": 15.033333, // Deschake, Neißeaue, Saxony
+		"lat_min":  47.270108, // Haldenwanger Eck, Oberstdorf, Bavaria
+		"lat_max":  54.9,      // Aventoft, Schleswig-Holstein
+	},
+}
