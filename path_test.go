bd6c2bc798a9f7192ac2665f51c3d250fb7f5bde
ane <ane@iki.fi>
2013-11-14 13:07:54 +0200

    New stuff!

--- path_test.go
+++ path_test.go
@@ -11,3 +11,12 @@ func TestPath(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func BenchmarkPath(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := CalculatePath(253299, 762749, "finland", 60)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
