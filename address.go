7f40cd957ae0b3e46e961a6360c68622287e5fec
ane <ane@iki.fi>
2014-01-10 18:16:02 +0200

    New DTO format for addresses, added CalculateCoordinatePath.

--- address.go
+++ address.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"database/sql"
+	"fmt"
 )
 
-type Address struct {
-	Street string
-	House string
-	Number string
-	City string
-	Country string
-	Coordinate Coord
+type Coordinate struct {
+	Latitude  float64
+	Longitude float64
 }
 
-func GetFuzzyAddress(config Config, address, country string, count int) ([]Address, error) {
+type Location struct {
+	Address    Address
+	Coordinate Coordinate
+}
+
+type Address struct {
+	Street      string
+	House       string
+	HouseNumber string
+	City        string
+	Country     string
+}
+
+func GetFuzzyAddress(config Config, address, country string, count int) ([]Location, error) {
 	newconf := Config(config)
 	newconf.DbName = "trgm_test"
 
@@ -23,31 +32,32 @@ func GetFuzzyAddress(config Config, address, country string, count int) ([]Addre
 		return nil, err
 	}
 
-
 	q := fmt.Sprintf("SELECT name, city, coord[0], coord[1] from get_appr('%s') LIMIT %d", address, count)
 	rows, err := db.Query(q)
 
 	if err != nil {
-		return []Address{}, err
+		return []Location{}, err
 	}
 
-	var addresses []Address
+	var locations []Location
 
 	for rows.Next() {
 		var (
 			street_name, city string
-			lat, long float64
+			lat, long         float64
 		)
 
 		if err := rows.Scan(&street_name, &city, &lat, &long); err != nil {
-			return []Address{}, err
+			return []Location{}, err
 		}
-		addresses = append(addresses, Address{Street: street_name, City: city, Coordinate: Coord{lat, long}})
+		locations = append(locations,
+			Location{Address{Street: street_name, City: city},
+				Coordinate{Latitude: lat, Longitude: long}})
 	}
 
 	if err := rows.Err(); err != nil {
-		return []Address{}, err
+		return []Location{}, err
 	}
 
-	return addresses, nil
+	return locations, nil
 }
--- path.go
+++ path.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/co-sky-developers/via/dmatrix"
+	"strings"
 )
 
 type Path struct {
 	Length int   `json:"length"`
 	Nodes  []int `json:"nodes"`
+}
+
+type CoordinatePath struct {
+	Length int     `json:"length"`
+	Coords []Coord `json:"coords"`
 }
 
 func CalculatePath(source, target int, country string, speed_profile int) (Path, error) {
@@ -34,6 +41,50 @@ func CalculatePath(source, target int, country string, speed_profile int) (Path,
 	if err := json.Unmarshal([]byte(res), &path); err != nil {
 		return Path{}, err
 	}
-
 	return path, nil
+}
+
+func IsMissingCoordinate(loc Location) bool {
+	if loc.Coordinate.Latitude == 0.0 || loc.Coordinate.Longitude == 0.0 {
+		return true
+	}
+	return false
+}
+
+func CalculateCoordinatePathFromAddresses(config Config, source, target Location, speed_profile int) (CoordinatePath, error) {
+	if IsMissingCoordinate(source) {
+		// resolve it
+		return CoordinatePath{}, errors.New("missing coord from source")
+	}
+	if IsMissingCoordinate(target) {
+		// resolve it
+		return CoordinatePath{}, errors.New("missing coord from target")
+	}
+
+	// step 1: coordinate -> node
+	srcLat, srcLong := source.Coordinate.Latitude, source.Coordinate.Longitude
+	trgLat, trgLong := target.Coordinate.Latitude, target.Coordinate.Longitude
+
+	srcNode, err := CorrectPoint(config, Coord{srcLat, srcLong}, strings.ToLower(source.Address.Country))
+	if err != nil {
+		return CoordinatePath{}, err
+	}
+	trgNode, err := CorrectPoint(config, Coord{trgLat, trgLong}, strings.ToLower(target.Address.Country))
+	if err != nil {
+		return CoordinatePath{}, err
+	}
+
+	// step 2: calculate path
+	path, err := CalculatePath(srcNode.Id, trgNode.Id, strings.ToLower(source.Address.Country), speed_profile)
+	if err != nil {
+		return CoordinatePath{}, err
+	}
+
+	// step 3: get coordinates
+	coordinateList, err := GetCoordinates(config, strings.ToLower(source.Address.Country), path.Nodes)
+	if err != nil {
+		return CoordinatePath{}, err
+	}
+
+	return CoordinatePath{Length: path.Length, Coords: coordinateList}, nil
 }
