c9943f409702b1f078a3779310290f8d1c35eea9
ane <ane@iki.fi>
2014-03-03 09:51:24 +0000

    Modify APIs to be correct.

--- api_test.go
+++ api_test.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+
+	"github.com/nfleet/via/geo"
 )
 
 type P struct {
-	matrix  []Coord
+	matrix  []geo.Coord
 	country string
 }
 
@@ -28,4 +35,44 @@ func TestBoundingBoxes(t *testing.T) {
 			t.Errorf("%d. check_coordinate_sanity(%q, %q) => %q, want %q", i, test.in.matrix, test.in.country, res, test.out)
 		}
 	}
+}
+
+func api_test_resolve(t *testing.T, locations []Location) {
+	request := fmt.Sprintf("http://localhost:%d/resolve", test_via.Config.Port)
+
+	jsonLoc, _ := json.Marshal(locations)
+	b := strings.NewReader(string(jsonLoc))
+
+	response, err := http.Post(request, "application/json", b)
+	if err != nil {
+		t.Fatal(response)
+	} else {
+		defer response.Body.Close()
+		cont, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var resolved []geo.Location
+		if err := json.Unmarshal(cont, &resolved); err != nil {
+			t.Fatal(string(cont), err)
+		}
+		for i, loc := range resolved {
+			t.Logf("Resolved %v to have coords %v", locations[i], loc)
+		}
+	}
+}
+
+func TestAPIResolvationCoordinate(t *testing.T) {
+	locations := []geo.Location{
+		{Address: geo.Address{Country: "finland"}, Coordinate: geo.Coordinate{Latitude: 62.24, Longitude: 25.74}},
+	}
+	api_test_resolve(t, locations)
+}
+
+func TestAPIResolvationWithoutCoordinate(t *testing.T) {
+	locations := []geo.Location{
+		{Address: geo.Address{Street: "Esplanadi", City: "Helsinki", Country: "finland"}},
+	}
+	api_test_resolve(t, locations)
 }
