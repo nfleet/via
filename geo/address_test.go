8ebec12f4f0f24e92c29c9f9f96145c1a28e1984
ane <ane@iki.fi>
2014-03-05 13:01:15 +0000

    Add address tests and benchmarks.

--- geo/address_test.go
+++ geo/address_test.go
@@ -6,12 +6,24 @@ import (
 	"github.com/nfleet/via/geotypes"
 )
 
-func TestResolvationForAddress(t *testing.T) {
-	locations := []geotypes.Location{
-		{Address: geotypes.Address{City: "Helsinki", Street: "Esplanadi", Country: "finland"}},
-		{Address: geotypes.Address{City: "Stuttgart", Street: "Calwer Straße", Country: "germany"}},
-	}
+var locations = []geotypes.Location{
+	{Address: geotypes.Address{City: "Helsinki", Street: "Esplanadi", Country: "finland"}},
+	{Address: geotypes.Address{City: "Stuttgart", Street: "Calwer Straße", Country: "germany"}},
+}
 
+func BenchmarkAddressFinlandResolvation(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		test_geo.ResolveLocation(locations[0])
+	}
+}
+
+func BenchmarkAddressGermanyResolvation(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		test_geo.ResolveLocation(locations[1])
+	}
+}
+
+func TestResolvationForAddress(t *testing.T) {
 	for _, loc := range locations {
 		resolvedLoc, err := test_geo.ResolveLocation(loc)
 		if err != nil {
