cefe8b30b81884d4b5fc3a2d3dc22757468c6e11
ane <ane@iki.fi>
2014-03-05 10:23:23 +0000

    Add tests for the configuration loading.

--- geo/geo_test.go
+++ geo/geo_test.go
@@ -1,9 +1,20 @@
 package geo
 
-import "github.com/nfleet/via/postgeodb"
+import (
+	"testing"
+
+	"github.com/nfleet/via/postgeodb"
+)
 
 var (
 	config, _ = LoadConfig("../development.json")
 	geoDB     = postgeodb.GeoPostgresDB{config}
 	test_geo  = NewGeo(true, geoDB, 3600)
 )
+
+func TestGeo(t *testing.T) {
+	_, err := LoadConfig("../development.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
